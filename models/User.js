1afe0cec4d2de703a7687177ee45fc1902748dbd
Carlos Delgado <carlos.dz16180@gmail.com>
2025-09-23 09:29:31 +0200

    Added favorite arrays to User DB model

--- models/User.js
+++ models/User.js
@@ -36,7 +36,24 @@ const user_schema = new mongoose.Schema({
     isVerified: {
         type: Boolean,
         default: false
-    }
-});
+    },
+
+    fav_commanderTech: [{
+        type: moongose.Schema.types.ObjectId,
+        ref: 'CommanderTech'
+    }],
+    fav_decklist: [{
+        type: moongose.Schema.types.ObjectId,
+        ref: 'Decklist'
+    }],
+    fav_user:[{
+        type: moongose.Schema.types.ObjectId,
+        ref: 'User'
+    }],
+    fav_tournament:[{
+        type: moongose.Schema.types.ObjectId,
+        ref: 'Tournament'
+    }]
+});     //I dont know if i would add a favorite option to all these 4 but i include them from the start to dodge consistency problems later on
 
 module.exports = mongoose.model('User', user_schema);
