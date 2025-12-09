189eb246227d2515b2e785e76c593a4ee595b47f
CarlosDZ <carlos.dz16180@gmail.com>
2025-12-09 13:28:40 +0100

    Feat: Mejora formulario login

--- frontend/views/Login.vue
+++ frontend/views/Login.vue
@@ -3,9 +3,43 @@
         <div id="loginFormBox">
             <h1>Iniciar sesión</h1>
             <form @submit.prevent="login">
-                <input placeholder="Username or Email" type="text" v-model="username" required />
-
-                <input placeholder="Password" type="password" v-model="password" required />
+                <div class="inputContainer">
+                    <span class="icon">
+                        <picture>
+                            <source
+                                srcset="../assets/images/user-darkmode.svg"
+                                media="(prefers-color-scheme: dark)"
+                            />
+                            <source
+                                srcset="../icons/user-light.svg"
+                                media="(prefers-color-scheme: light)"
+                            />
+                            <img src="../assets/images/user-lightmode.svg" alt="User icon" />
+                        </picture>
+                    </span>
+                    <input
+                        placeholder="Username or Email"
+                        type="text"
+                        v-model="username"
+                        required
+                    />
+                </div>
+                <div class="inputContainer">
+                    <span class="icon">
+                        <picture>
+                            <source
+                                srcset="../assets/images/lock-darkmode.svg"
+                                media="(prefers-color-scheme: dark)"
+                            />
+                            <source
+                                srcset="../icons/user-light.svg"
+                                media="(prefers-color-scheme: light)"
+                            />
+                            <img src="../assets/images/lock-lightmode.svg" alt="User icon" />
+                        </picture>
+                    </span>
+                    <input placeholder="Password" type="password" v-model="password" required />
+                </div>
 
                 <button type="submit">ENTRAR</button>
             </form>
