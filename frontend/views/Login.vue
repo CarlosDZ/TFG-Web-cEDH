0417db48aff67ef9a5170b23aba2ead56ec7f8d2
CarlosDZ <carlos.dz16180@gmail.com>
2025-12-10 09:37:40 +0100

    Feat: Login funciona (falta cookie cors etc)

--- frontend/views/Login.vue
+++ frontend/views/Login.vue
@@ -11,7 +11,7 @@
                                 media="(prefers-color-scheme: dark)"
                             />
                             <source
-                                srcset="../icons/user-light.svg"
+                                srcset="../assets/images/user-lightmode.svg"
                                 media="(prefers-color-scheme: light)"
                             />
                             <img src="../assets/images/user-lightmode.svg" alt="User icon" />
@@ -21,6 +21,7 @@
                         placeholder="Username or Email"
                         type="text"
                         v-model="username"
+                        id="username"
                         required
                     />
                 </div>
@@ -32,13 +33,19 @@
                                 media="(prefers-color-scheme: dark)"
                             />
                             <source
-                                srcset="../icons/user-light.svg"
+                                srcset="../assets/images/lock-lightmode.svg"
                                 media="(prefers-color-scheme: light)"
                             />
                             <img src="../assets/images/lock-lightmode.svg" alt="User icon" />
                         </picture>
                     </span>
-                    <input placeholder="Password" type="password" v-model="password" required />
+                    <input
+                        placeholder="Password"
+                        type="password"
+                        v-model="password"
+                        id="password"
+                        required
+                    />
                 </div>
 
                 <button type="submit">ENTRAR</button>
@@ -49,11 +56,24 @@
 
 <script setup>
 import { ref } from "vue";
-
-const username = ref("");
-const password = ref("");
-
+import axios from "axios";
+import dotenv from "dotenv";
 function login() {
-    // Process data and call backend
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    const backend_url = import.meta.env.VITE_BACKEND_URL;
+    // Add shitty comprobations
+
+    axios
+        .post(`http://${backend_url}/api/auth/login`, {
+            nameOrMail: username,
+            password: password,
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 </script>
