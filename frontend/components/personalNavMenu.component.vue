4200d3717cb44d340e45cabb6f34812e17fc0ab7
CarlosDZ <carlos.dz16180@gmail.com>
2026-01-23 07:01:38 +0100

    FEAT: Operative menu toggle

--- frontend/components/personalNavMenu.component.vue
+++ frontend/components/personalNavMenu.component.vue
@@ -1,36 +1,50 @@
 <script setup>
 import { authState } from "../utils/auth";
-
+import { ref } from "vue";
 const auth = authState();
+
+const isOpen = ref(true);
+
+function togglePanel() {
+    isOpen.value = !isOpen.value;
+}
 </script>
 
 <template>
-    <div v-if="!auth.isLogged">
-        <h1>Estas en modo invitado... Inicia sesion para ver tu area personal.</h1>
-        <RouterLink to="/login"
-            ><button><h1>Iniciar Sesion</h1></button></RouterLink
-        >
-        <h2>o si no tienes una cuenta...</h2>
-        <RouterLink to="/register"
-            ><button><h1>Crear Cuenta</h1></button></RouterLink
-        >
-    </div>
-    <div v-else>
-        <RouterLink to="/torneos"
-            ><button><h1>Torneos</h1></button></RouterLink
-        >
-        <!--Cambiar todo esto cuando haga las vistas del area personal-->
-        <RouterLink to="/decklists"
-            ><button><h1>Decklists</h1></button></RouterLink
-        >
-        <!--Cambiar todo esto cuando haga las vistas del area personal-->
-        <RouterLink to="/commander-techs"
-            ><button><h1>Commander Techs</h1></button></RouterLink
-        >
-        <!--Cambiar todo esto cuando haga las vistas del area personal-->
-        <RouterLink to="/jugadores"
-            ><button><h1>Jugadores</h1></button></RouterLink
-        >
-        <!--Cambiar todo esto cuando haga las vistas del area personal-->
+    <div id="mainContainer-personalNavMenu" :class="{ open: isOpen }">
+        <div class="tab" @click="togglePanel">
+            <span>{{ isOpen ? ">" : "<" }}</span>
+        </div>
+
+        <div class="content">
+            <div v-if="!auth.isLogged">
+                <h1>Estas en modo invitado... Inicia sesion para ver tu area personal.</h1>
+                <RouterLink to="/login"
+                    ><button><h1>Iniciar Sesion</h1></button></RouterLink
+                >
+                <h2>o si no tienes una cuenta...</h2>
+                <RouterLink to="/register"
+                    ><button><h1>Crear Cuenta</h1></button></RouterLink
+                >
+            </div>
+            <div v-else>
+                <RouterLink to="/torneos"
+                    ><button><h1>Torneos</h1></button></RouterLink
+                >
+                <!--Cambiar todo esto cuando haga las vistas del area personal-->
+                <RouterLink to="/decklists"
+                    ><button><h1>Decklists</h1></button></RouterLink
+                >
+                <!--Cambiar todo esto cuando haga las vistas del area personal-->
+                <RouterLink to="/commander-techs"
+                    ><button><h1>Commander Techs</h1></button></RouterLink
+                >
+                <!--Cambiar todo esto cuando haga las vistas del area personal-->
+                <RouterLink to="/jugadores"
+                    ><button><h1>Jugadores</h1></button></RouterLink
+                >
+                <!--Cambiar todo esto cuando haga las vistas del area personal-->
+            </div>
+        </div>
     </div>
 </template>
