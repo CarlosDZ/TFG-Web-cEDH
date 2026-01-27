374774799f1d8ed87a7e1d961eec649b9d35ead0
CarlosDZ <carlos.dz16180@gmail.com>
2026-01-27 10:06:30 +0100

    STYLE: Style-Structure Connection

--- frontend/components/sectionNavMenu.component.vue
+++ frontend/components/sectionNavMenu.component.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="mainContainer-sectioNavMenu">
     <RouterLink to="/torneos"><button><h1>Torneos</h1></button></RouterLink>
     <RouterLink to="/decklists"><button><h1>Decklists</h1></button></RouterLink>
     <RouterLink to="/commander-techs"><button><h1>Commander Techs</h1></button></RouterLink>
