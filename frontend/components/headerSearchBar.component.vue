ea2e7335cae004a231b8ac7e62cd5426db7c6d94
CarlosDZ <carlos.dz16180@gmail.com>
2026-01-28 06:46:20 +0100

    FIX: Ported monolitic code to the component

---
+++ frontend/components/headerSearchBar.component.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+    <div id="mainContainer-headerSearchBar">
+        <img src="../assets/images/texture_placeholder.jpg" alt="Spain cEDH" />
+
+        <div class="search-area">
+            <input type="text" placeholder="Escribe aquí para buscar..." />
+            <select>
+                <option value="card">Cartas</option>
+                <option value="deck">Decks</option>
+                <option value="commander-tech">Techs</option>
+                <option value="user">Jugadores</option>
+                <option value="discussion">Discusiones</option>
+            </select>
+            <button class="search-btn"><b>Buscar</b></button>
+        </div>
+
+        <button id="profile-button">
+            <img src="../assets/images/avatar.jpg" alt="Prof" />
+        </button>
+    </div>
+</template>
--- frontend/views/Dashboard.vue
+++ frontend/views/Dashboard.vue
@@ -6,6 +6,7 @@ import { authState } from "../utils/auth";
 import mainFeed from "../components/discussions/mainFeed.component.vue";
 import sectionNavMenu from "../components/sectionNavMenu.component.vue";
 import personalNavMenu from "../components/personalNavMenu.component.vue";
+import headersSearchBar from "../components/headerSearchBar.component.vue";
 
 const backend_url = import.meta.env.VITE_BACKEND_URL;
 const auth = authState();
@@ -14,23 +15,7 @@ const auth = authState();
 <template>
     <div id="mainContainer-dashboard">
         <header>
-            <img src="../assets/images/texture_placeholder.jpg" alt="Spain cEDH" />
-
-            <div class="search-area">
-                <input type="text" placeholder="Escribe aquí para buscar..." />
-                <select>
-                    <option value="card">Cartas</option>
-                    <option value="deck">Decks</option>
-                    <option value="commander-tech">Techs</option>
-                    <option value="user">Jugadores</option>
-                    <option value="discussion">Discusiones</option>
-                </select>
-                <button class="search-btn"><b>Buscar</b></button>
-            </div>
-
-            <button id="profile-button">
-                <img src="../assets/images/avatar.jpg" alt="Prof" />
-            </button>
+            <headersSearchBar />
         </header>
 
         <main>
