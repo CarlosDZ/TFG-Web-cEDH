a60cd2ec2d2bd0c485cc64e816b895b31afa9ce1
CarlosDZ <carlos.dz16180@gmail.com>
2026-03-31 03:14:22 +0200

    STYLE: Redesigned footer nav menu

--- frontend/components/sectionNavMenu.component.vue
+++ frontend/components/sectionNavMenu.component.vue
@@ -4,76 +4,81 @@ import { name, version, author } from "../../package.json" with { type: "json" }
 
 <template>
     <div id="mainContainer-sectionNavMenu">
-        <RouterLink to="/torneos" class="bttnWrapper"><h1>Torneos</h1></RouterLink>
-        <RouterLink to="/decklists" class="bttnWrapper"><h1>Decklists</h1></RouterLink>
-        <RouterLink to="/commander-techs" class="bttnWrapper"><h1>Commander Techs</h1></RouterLink>
-        <RouterLink to="/jugadores" class="bttnWrapper"><h1>Jugadores</h1></RouterLink>
-
+        <RouterLink to="/torneos" class="bttnWrapper"><span>Torneos</span></RouterLink>
+        <RouterLink to="/decklists" class="bttnWrapper"><span>Decklists</span></RouterLink>
+        <RouterLink to="/commander-techs" class="bttnWrapper">
+            <span>Commander Techs</span>
+        </RouterLink>
+        <RouterLink to="/jugadores" class="bttnWrapper"><span>Jugadores</span></RouterLink>
         <div id="txt-wrapper">
-            <h3>{{ name }} {{ version }}</h3>
-            <h4>Developed by {{ author }}</h4>
+            <p class="app-name">{{ name }} {{ version }}</p>
+            <p class="app-author">Developed by {{ author }}</p>
         </div>
     </div>
 </template>
 
 <style scoped>
 #mainContainer-sectionNavMenu {
+    --muted: color-mix(in srgb, var(--txt-color) 45%, transparent);
+
     display: flex;
     width: 100%;
     height: 100%;
     position: absolute;
-    padding-left: 20px;
-
+    padding-left: 16px;
     background-color: var(--background-color);
     overflow: hidden;
+    align-items: center;
+}
 
-    .bttnWrapper {
-        width: auto;
-        display: flex;
-        min-width: 13%;
-        height: 100%;
-        margin-right: 1%;
-        align-items: center;
-        justify-content: center;
-        background-color: var(--button-background-color);
-        transform: skewX(-25deg);
-        text-decoration: none;
-        overflow: hidden;
-        transition:
-            transform 0.8s ease,
-            background 0.8s ease;
-        h1 {
-            transform: skewX(25deg);
-            align-items: center;
-            justify-content: center;
-            color: var(--txt-color);
-            padding: 0 30px;
-        }
+.bttnWrapper {
+    min-width: 13%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--button-background-color);
+    transform: skewX(-25deg);
+    text-decoration: none;
+    overflow: hidden;
+    margin-right: 1%;
+    transition:
+        transform 0.35s ease,
+        background 0.35s ease;
+
+    span {
+        transform: skewX(25deg);
+        font-size: 14px;
+        font-weight: 500;
+        color: var(--txt-color);
+        padding: 0 28px;
+        white-space: nowrap;
     }
+}
 
-    .bttnWrapper:hover {
-        transform: scale(1.1) skewX(-25deg);
-        overflow-y: hidden;
-        background-color: var(--button-background-color-hovered);
-        h1 {
-            transform: skewX(25deg);
-        }
-    }
+.bttnWrapper:hover {
+    transform: scale(1.08) skewX(-25deg);
+    background-color: var(--button-background-color-hovered);
+}
 
+#txt-wrapper {
+    margin-left: auto;
+    margin-right: 3%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    gap: 1px;
+}
 
-    #txt-wrapper{
-        margin-left: auto;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin-right: 3%;
+.app-name {
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--txt-color);
+}
 
-        h3, h4{
-            text-align: center;
-            color: var(--txt-color);
-        }
-        
-    }
+.app-author {
+    font-size: 10px;
+    color: var(--muted);
 }
 </style>
