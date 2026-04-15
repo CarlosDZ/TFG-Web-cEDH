a1d54f4fc5a2a7ca8e13c3a28baf55b28a2cd7b9
CarlosDZ <carlos.dz16180@gmail.com>
2026-04-16 01:11:01 +0200

    PCKG: Testing dependencies for frontend

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@ const { defineConfig } = require("vite");
 const vue = require("@vitejs/plugin-vue");
 
 module.exports = defineConfig({
-    root: "./frontend",
-    plugins: [vue()],
-    build: {
-        outDir: "../dist",
+  root: "./frontend",
+  plugins: [vue()],
+  build: {
+    outDir: "../dist",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: `http://localhost:${process.env.BACKEND_PORT || 3000}`,
+        changeOrigin: true,
+      },
     },
-    server: {
-        proxy: {
-            "/api": {
-                target: `http://localhost:${process.env.BACKEND_PORT || 3000}`,
-                changeOrigin: true,
-            },
-        },
-    },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    root: ".",
+    include: ["frontend/**/*.test.js"],
+  },
 });
